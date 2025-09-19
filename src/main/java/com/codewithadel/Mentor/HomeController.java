28752da7b5ec9228427c5127ddb2d81ef1c19adf
firstPush <adel.sdet2@gmail.com>
2025-09-19 10:11:31 -0500

    added dependencies; resolved all conflicts for jpa and Thymeleaf

--- src/main/java/com/codewithadel/Mentor/HomeController.java
+++ src/main/java/com/codewithadel/Mentor/HomeController.java
@@ -1,13 +1,14 @@
 package com.codewithadel.Mentor;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
-        return "index.html";
+        return "index";
 
     }
 }
