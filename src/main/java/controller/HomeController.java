fa7ce97165eb2cf22f75f77217db762a3dbe26d5
firstPush <adel.sdet2@gmail.com>
2025-09-22 16:27:00 -0500

    foundational package structure

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.codewithadel.Mentor;
+package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +9,6 @@ public class HomeController {
     @GetMapping("/")
     public String index() {
         return "index";
-
+//
     }
 }
